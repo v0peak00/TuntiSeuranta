d433fe91d89afbe5971d51e7a4f8b752b9c684d3
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-09-05 12:07:54 +0300

    refactor: Convert validation from InputHandler to ValidationHelper

--- InputHandling/Validators/IInputValidator.cs
+++
@@ -1,7 +0,0 @@
-namespace TuntiSeuranta.InputHandling.Validators;
-
-public interface IInputValidator
-{
-    void SetNext(IInputValidator nextValidator);
-    bool Validate(string input, out string errorMessage);
-}
