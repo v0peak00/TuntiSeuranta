d17cb747eb33407fa917ff2535bafd2f1980d73f
Aki Peltokangas <v0peak00@students.oamk.fi>
2024-09-05 12:15:30 +0300

    refactor: Refactor SessionManager to handle orchestration of actions
    - Move business logic for using the user input from InputHandler to SessionManager, now InputHandler
        is only concerned about user input and using validationHelper for input validation.
    
    - Now prompts for user input with consoleHandler, handles the input with InputHandler,
        and shows the user prompts when needed.

--- SessionManager.cs
+++ SessionManager.cs
@@ -1,16 +1,20 @@
+using TuntiSeuranta.ConsoleHandling;
 using TuntiSeuranta.InputHandling;
 using TuntiSeuranta.WorkHoursManagement;
 
 public class SessionManager
 {
-    private readonly InputHandler _inputHandler;
     private readonly WorkHoursDisplay _workHoursDisplay;
+    private readonly WorkHoursService _workHoursService;
+    private readonly InputHandler _inputHandler;
+    private readonly IConsoleHandler _consoleHandler;
 
-    public SessionManager(InputHandler inputHandler, WorkHoursDisplay workHoursDisplay)
+    public SessionManager(WorkHoursService workHoursService, WorkHoursDisplay workHoursDisplay, InputHandler inputHandler, IConsoleHandler consoleHandler)
     {
-        _inputHandler = inputHandler;
-
-        _workHoursDisplay = workHoursDisplay;
+        _workHoursService = workHoursService ?? throw new ArgumentNullException(nameof(workHoursService));
+        _workHoursDisplay = workHoursDisplay ?? throw new ArgumentNullException(nameof(workHoursDisplay));
+        _inputHandler = inputHandler ?? throw new ArgumentNullException(nameof(inputHandler));
+        _consoleHandler = consoleHandler ?? throw new ArgumentNullException(nameof(consoleHandler));
     }
 
     public void RunSession()
@@ -19,12 +23,76 @@ public class SessionManager
         {
             _workHoursDisplay.DisplayPreviousHours();
 
-            if (!_inputHandler.TryGetDateInput(out DateTime dateInput)) return;
-
-            if (!_inputHandler.HandleDateInput(dateInput))
+            DateTime dateInput = _inputHandler.GetValidatedDate("Syötä PVM (pp.kk.vvvv) tai 'q' palataksesi");
+            if (dateInput == default)
             {
-                return; // Exit if user chose to quit
+                return;
+            }
+
+            if (_workHoursService.WorkHoursExistsForDate(dateInput))
+            {
+                HandleExistingWorkHours(dateInput);
+            }
+            else
+            {
+                HandleAddNewWorkHours(dateInput);
             }
         }
+    }
+
+    private void HandleExistingWorkHours(DateTime dateInput)
+    {
+        _consoleHandler.WriteMessage($"Tunteja löytyi PVM: {dateInput:dd.MM.yyyy}");
+        _workHoursDisplay.DisplayHoursForDate(dateInput);
+
+        bool modifyHours = _inputHandler.GetModifyHoursSelection(
+            "Haluatko muokata näitä tunteja? Y/N tai 'q' palataksesi päävalikkoon: "
+        );
+
+        if (!modifyHours)
+        {
+            _consoleHandler.ExitModifyingHours();
+            return;
+        }
+
+        HandleModifyWorkHours(dateInput);
+    }
+
+    private void HandleModifyWorkHours(DateTime dateInput)
+    {
+        if (GetValidatedStartAndEndTimes(out TimeSpan startTimeInput, out TimeSpan endTimeInput))
+        {
+            _workHoursService.ModifyWorkHours(dateInput, startTimeInput, endTimeInput);
+            _consoleHandler.WriteMessage($"Työaika päivitetty päivälle {dateInput:dd.MM.yyyy}");
+        }
+    }
+
+    private void HandleAddNewWorkHours(DateTime dateInput)
+    {
+        if (GetValidatedStartAndEndTimes(out TimeSpan startTimeInput, out TimeSpan endTimeInput))
+        {
+            _workHoursService.AddNewWorkHours(dateInput, startTimeInput, endTimeInput);
+            _consoleHandler.WriteMessage($"Uudet työajat lisätty päivälle {dateInput:dd.MM.yyyy}");
+        }
+    }
+
+    private bool GetValidatedStartAndEndTimes(out TimeSpan startTimeInput, out TimeSpan endTimeInput)
+    {
+        startTimeInput = _inputHandler.GetValidatedTime("Syötä aloitus aika (hh:mm) tai 'q' palataksesi.");
+        endTimeInput = default;
+        if (startTimeInput == default)
+        {
+            _consoleHandler.ExitModifyingHours();
+            return false;
+        }
+
+        endTimeInput = _inputHandler.GetValidatedTime("Syötä lopetus aika (hh:mm) tai 'q' palataksesi.");
+        if (endTimeInput == default)
+        {
+            _consoleHandler.ExitModifyingHours();
+            return false;
+        }
+
+        return true;
     }
 }
